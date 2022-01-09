8553b6fab9bdf91b2fc43e144a6658a75988d355
Oluwaseun Aderinlokun <aderinlokunoluwaseun@gmail.com>
2022-01-09 15:31:07 +0100

    feat: Fetch single task

--- taskservice/task.route.js
+++ taskservice/task.route.js
@@ -7,6 +7,7 @@ const { taskPayloadValidation } = require("./task.middleware");
 const router = express.Router();
 
 router.get("/all", authMiddleware, taskController.retrieveAll);
+router.get("/:id", authMiddleware, taskController.retrieve);
 router.post("/", authMiddleware, taskPayloadValidation, taskController.create);
 router.patch("/", taskPayloadValidation, authMiddleware, taskController.update);
 router.delete("/", authMiddleware, taskController.remove);
